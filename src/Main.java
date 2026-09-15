3a1918ddf1291329d045723643f487525ee7b200
bachdang3008 <bdang30082005@gmail.com>
2026-09-15 23:19:58 +0700

    5

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@ void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     IO.println(String.format("Hello and welcome!"));
-    int i =5;
-    int a;
+    int a =5;
 
     for (int i = 1; i <= 5; i++) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
